51aaa57323d5f8dc6d656da35b0a2ea5a1aefb80
dianawhalen <whalendianaj@gmail.com>
2019-07-15 19:09:36 -0600

    add seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -14,18 +14,23 @@
 ActiveRecord::Schema.define(version: 20190703171546) do
 
   create_table "designers", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "toys", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
